1c15546479e21c67eeed5769eb94760b159b04ba
crnutriplus <info@crnutriplus.com>
2026-09-06 19:34:57 -0600

    test: cover inbox-wide incoming notifications

--- spec/services/messages/new_message_notification_service_spec.rb
+++ spec/services/messages/new_message_notification_service_spec.rb
@@ -20,6 +20,7 @@ describe Messages::NewMessageNotificationService do
     let(:assignee) { create(:user, account: account) }
     let(:participating_agent_1) { create(:user, account: account) }
     let(:participating_agent_2) { create(:user, account: account) }
+    let(:inbox_only_agent) { create(:user, account: account) }
     let(:inbox) { create(:inbox, account: account) }
     let(:conversation) { create(:conversation, account: account, inbox: inbox, assignee: assignee) }
 
@@ -27,6 +28,7 @@ describe Messages::NewMessageNotificationService do
       create(:inbox_member, inbox: inbox, user: participating_agent_1)
       create(:inbox_member, inbox: inbox, user: participating_agent_2)
       create(:inbox_member, inbox: inbox, user: assignee)
+      create(:inbox_member, inbox: inbox, user: inbox_only_agent)
       create(:conversation_participant, conversation: conversation, user: participating_agent_1)
       create(:conversation_participant, conversation: conversation, user: participating_agent_2)
       create(:conversation_participant, conversation: conversation, user: assignee)
@@ -57,7 +59,7 @@ describe Messages::NewMessageNotificationService do
     end
 
     context 'when message is created by a contact' do
-      let(:message) { create(:message, conversation: conversation, account: account) }
+      let(:message) { create(:message, conversation: conversation, account: account, message_type: :incoming) }
 
       before do
         described_class.new(message: message).perform
@@ -76,16 +78,38 @@ describe Messages::NewMessageNotificationService do
                                                          account: account, primary_actor: message.conversation,
                                                          secondary_actor: message)).to exist
       end
+
+      it 'creates a notification for an inbox member who is not assigned or participating' do
+        expect(inbox_only_agent.notifications.where(notification_type: 'participating_conversation_new_message',
+                                                    account: account, primary_actor: message.conversation,
+                                                    secondary_actor: message)).to exist
+      end
+    end
+
+    context 'when an incoming message is created on an unassigned conversation' do
+      let(:message) { create(:message, conversation: conversation, account: account, message_type: :incoming) }
+
+      before do
+        conversation.update!(assignee: nil)
+        described_class.new(message: message).perform
+      end
+
+      it 'creates notifications for every inbox member' do
+        [assignee, participating_agent_1, participating_agent_2, inbox_only_agent].each do |agent|
+          expect(agent.notifications.where(account: account, primary_actor: message.conversation,
+                                           secondary_actor: message)).to exist
+        end
+      end
     end
 
     context 'when multiple notification conditions are met' do
-      let(:message) { create(:message, conversation: conversation, account: account) }
+      let(:message) { create(:message, conversation: conversation, account: account, message_type: :incoming) }
 
       before do
         described_class.new(message: message).perform
       end
 
-      it 'will not create participating notifications for the assignee if assignee notification was send' do
+      it 'will not create participating notifications for the assignee if assignee notification was sent' do
         expect(assignee.notifications.where(notification_type: 'assigned_conversation_new_message',
                                             account: account, primary_actor: message.conversation,
                                             secondary_actor: message)).to exist
@@ -93,10 +117,17 @@ describe Messages::NewMessageNotificationService do
                                             account: account, primary_actor: message.conversation,
                                             secondary_actor: message)).not_to exist
       end
+
+      it 'creates only one notification per agent for the same message' do
+        expect(assignee.notifications.where(account: account, primary_actor: message.conversation,
+                                            secondary_actor: message).count).to eq(1)
+        expect(participating_agent_1.notifications.where(account: account, primary_actor: message.conversation,
+                                                         secondary_actor: message).count).to eq(1)
+      end
     end
 
     context 'when message is created by assignee' do
-      let(:message) { create(:message, conversation: conversation, account: account, sender: assignee) }
+      let(:message) { create(:message, conversation: conversation, account: account, sender: assignee, message_type: :outgoing) }
 
       before do
         described_class.new(message: message).perform
@@ -109,6 +140,11 @@ describe Messages::NewMessageNotificationService do
         expect(assignee.notifications.where(notification_type: 'assigned_conversation_new_message',
                                             account: account, primary_actor: message.conversation,
                                             secondary_actor: message)).not_to exist
+      end
+
+      it 'does not notify unrelated inbox members for outgoing messages' do
+        expect(inbox_only_agent.notifications.where(account: account, primary_actor: message.conversation,
+                                                    secondary_actor: message)).not_to exist
       end
     end
   end
